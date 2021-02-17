eed0f39962dea9bda9091b1eb1c42c48cc259b9e
bob_home <chanmin9401@gmail.com>
2021-02-17 21:56:42 +0900

    add presets targets, plugins, devserver

--- React/WordRelay/webpack.config.js
+++ React/WordRelay/webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name : 'word-relay-setting',
     mode : 'development', // 실서비스 : production
-    devtool : 'eval',
+    devtool : 'eval',   // 실서비스 : hidden-source-map
 
     resolve : {
         extensions: ['.js', '.jsx']
@@ -19,14 +20,30 @@ module.exports = {
             test : /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets:['@babel/preset-env', '@babel/preset-react'],
-                plugins:['@babel/plugin-proposal-class-properties']
+                presets:[
+                    ['@babel/preset-env',{
+                        targets : {
+                            browsers : ['> 1% in KR']    // preset을 적용할 브라우저의 종류를 정의할 수 있다.
+                        }
+                    }],
+                    '@babel/preset-react'],   // plugin들의 모음이 preset
+                plugins:[
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel'
+                ]
             }
         }],
     },
-
+    plugins : [
+        new RefreshWebpackPlugin()
+    ],
     output: {
         path : path.join(__dirname, 'dist'),
         filename : 'app.js'
+    },
+
+    devServer:{
+        publicPath : '/dist',
+        hot : true
     }
 };
