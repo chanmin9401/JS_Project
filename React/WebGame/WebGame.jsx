717279a71b8e4977e9d66c79f761ce06ebfd0330
bob_home <chanmin9401@gmail.com>
2021-02-26 14:06:53 +0900

    add NumberBaseBall Game

--- React/WebGame/WebGame.jsx
+++ React/WebGame/WebGame.jsx
@@ -2,6 +2,7 @@ const React = require('react');
 const { useState } = React;
 const WordRelay = require('./WordRelay');
 const GuGudan = require('./GuGudan');
+const NumberBaseBall = require('./NumberBaseBall');
 const {} = require('./style')
 
 const WebGame = () => {
@@ -13,6 +14,8 @@ const WebGame = () => {
             setTabCode('WordRelay');
         }else if(tabCd === 'GG'){
             setTabCode('GuGudan');
+        }else if(tabCd === 'NBB'){
+            setTabCode('NumberBaseBall');
         }
     }
 
@@ -21,8 +24,10 @@ const WebGame = () => {
         <div className='exp'>게임이름을 클릭하면 게임이 변경됩니다.</div>
         <div className='tab' onClick={() => onClickTab('WR')}>끝말잇기</div>
         <div className='tab'onClick={() => onClickTab('GG')}>구구단</div>
+        <div className='tab'onClick={() => onClickTab('NBB')}>숫자야구</div>
             {tabCode === 'WordRelay' && <WordRelay />}
             {tabCode === 'GuGudan' && <GuGudan />}
+            {tabCode === 'NumberBaseBall' && <NumberBaseBall />}
         </>
     );
 }
